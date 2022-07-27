98310a760303fcfa45ca90ce9c118f433713fb71
Charles Dixon <chvckd@gmail.com>
2022-07-27 15:20:14 +0100

    Add remaining (non-replica) core operations to test-client

--- test-client/cas.go
+++ test-client/cas.go
@@ -1,3 +1,16 @@
 package gocbps
 
+import "github.com/couchbase/stellar-nebula/protos"
+
 type Cas uint64
+
+func (cas Cas) toProto() *protos.Cas {
+	if cas == 0 {
+		return nil
+	}
+
+	protosCas := &protos.Cas{
+		Value: uint64(cas),
+	}
+	return protosCas
+}
--- test-client/collection.go
+++ test-client/collection.go
@@ -3,6 +3,8 @@ package gocbps
 import (
 	"context"
 	"github.com/couchbase/stellar-nebula/protos"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"time"
 )
 
 type Collection struct {
@@ -45,6 +47,111 @@ func (c *Collection) Get(ctx context.Context, id string, opts *GetOptions) (*Get
 		Content: resp.Content,
 		Cas:     Cas(resp.Cas.Value),
 	}, nil
+}
+
+type ExistsOptions struct {
+}
+
+type ExistsResult struct {
+	Exists bool
+	Cas    Cas
+}
+
+func (c *Collection) Exists(ctx context.Context, id string, opts *ExistsOptions) (*ExistsResult, error) {
+	if opts == nil {
+		opts = &ExistsOptions{}
+	}
+	client, bucketName, scopeName, collName := c.getClient()
+
+	resp, err := client.couchbaseClient.Exists(ctx, &protos.ExistsRequest{
+		BucketName:     bucketName,
+		ScopeName:      scopeName,
+		CollectionName: collName,
+		Key:            id,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &ExistsResult{
+		Exists: resp.Result,
+		Cas:    Cas(resp.Cas.Value),
+	}, nil
+}
+
+type GetAndTouchOptions struct {
+}
+
+func (c *Collection) GetAndTouch(ctx context.Context, id string, expiry time.Time, opts *GetAndTouchOptions) (*GetResult, error) {
+	if opts == nil {
+		opts = &GetAndTouchOptions{}
+	}
+	client, bucketName, scopeName, collName := c.getClient()
+
+	resp, err := client.couchbaseClient.GetAndTouch(ctx, &protos.GetAndTouchRequest{
+		BucketName:     bucketName,
+		ScopeName:      scopeName,
+		CollectionName: collName,
+		Key:            id,
+		Expiry:         timestamppb.New(expiry),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetResult{
+		Content: resp.Content,
+		Cas:     Cas(resp.Cas.Value),
+	}, nil
+}
+
+type GetAndLockOptions struct {
+}
+
+func (c *Collection) GetAndLock(ctx context.Context, id string, lockTime time.Duration, opts *GetAndLockOptions) (*GetResult, error) {
+	if opts == nil {
+		opts = &GetAndLockOptions{}
+	}
+	client, bucketName, scopeName, collName := c.getClient()
+
+	resp, err := client.couchbaseClient.GetAndLock(ctx, &protos.GetAndLockRequest{
+		BucketName:     bucketName,
+		ScopeName:      scopeName,
+		CollectionName: collName,
+		Key:            id,
+		LockTime:       uint32(lockTime / time.Second),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &GetResult{
+		Content: resp.Content,
+		Cas:     Cas(resp.Cas.Value),
+	}, nil
+}
+
+type UnlockOptions struct {
+}
+
+func (c *Collection) Unlock(ctx context.Context, id string, cas Cas, opts *UnlockOptions) error {
+	if opts == nil {
+		opts = &UnlockOptions{}
+	}
+	client, bucketName, scopeName, collName := c.getClient()
+
+	_, err := client.couchbaseClient.Unlock(ctx, &protos.UnlockRequest{
+		BucketName:     bucketName,
+		ScopeName:      scopeName,
+		CollectionName: collName,
+		Key:            id,
+		Cas:            cas.toProto(),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type UpsertOptions struct {
@@ -151,6 +258,31 @@ func (c *Collection) Remove(ctx context.Context, id string, opts *RemoveOptions)
 		CollectionName:  collName,
 		Key:             id,
 		DurabilityLevel: opts.DurabilityLevel.toProto(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &MutationResult{
+		Cas: Cas(resp.Cas.Value),
+	}, nil
+}
+
+type TouchOptions struct {
+}
+
+func (c *Collection) Touch(ctx context.Context, id string, expiry time.Time, opts *TouchOptions) (*MutationResult, error) {
+	if opts == nil {
+		opts = &TouchOptions{}
+	}
+	client, bucketName, scopeName, collName := c.getClient()
+
+	resp, err := client.couchbaseClient.Touch(ctx, &protos.TouchRequest{
+		BucketName:     bucketName,
+		ScopeName:      scopeName,
+		CollectionName: collName,
+		Key:            id,
+		Expiry:         timestamppb.New(expiry),
 	})
 	if err != nil {
 		return nil, err
