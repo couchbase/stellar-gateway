c655ddb0bd5bde23d3188f2c8e180bbc5ba48c91
Brett Lawson <brett19@gmail.com>
2022-10-09 13:19:43 -0700

    Cleaned up protostellar generation.

--- generate-protos.go
+++ generate-protos.go
@@ -1,4 +1,13 @@
 //go:generate mkdir -p ./genproto
-//go:generate protoc --go_out=./genproto --go_opt=module=github.com/couchbase/stellar-nebula/genproto --go-grpc_out=./genproto --go-grpc_opt=module=github.com/couchbase/stellar-nebula/genproto proto/com.couchbase.v1.proto proto/com.couchbase.data.v1.proto proto/com.couchbase.query.v1.proto proto/com.couchbase.search.v1.proto proto/com.couchbase.analytics.v1.proto proto/com.couchbase.view.v1.proto proto/com.couchbase.transactions.v1.proto proto/com.couchbase.routing.v1.proto proto/com.couchbase.admin.bucket.v1.proto
+//go:generate -command protostellar protoc --go_out=./genproto --go_opt=module=github.com/couchbase/stellar-nebula/genproto --go-grpc_out=./genproto --go-grpc_opt=module=github.com/couchbase/stellar-nebula/genproto
+//go:generate protostellar proto/com.couchbase.v1.proto
+//go:generate protostellar proto/com.couchbase.data.v1.proto
+//go:generate protostellar proto/com.couchbase.query.v1.proto
+//go:generate protostellar proto/com.couchbase.search.v1.proto
+//go:generate protostellar proto/com.couchbase.analytics.v1.proto
+//go:generate protostellar proto/com.couchbase.view.v1.proto
+//go:generate protostellar proto/com.couchbase.transactions.v1.proto
+//go:generate protostellar proto/com.couchbase.routing.v1.proto
+//go:generate protostellar proto/com.couchbase.admin.bucket.v1.proto
 
 package main
