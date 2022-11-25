0c1a4297ed44dc1b037a49f579be1b2c3443c0fe
Brett Lawson <brett19@gmail.com>
2022-11-25 14:20:51 -0800

    Fixed a missed name when renaming 'data' to 'kv'.

--- generate-protos.go
+++ generate-protos.go
@@ -1,7 +1,7 @@
 //go:generate mkdir -p ./genproto
 //go:generate -command protostellar protoc --go_out=./genproto --go_opt=module=github.com/couchbase/stellar-nebula/genproto --go-grpc_out=./genproto --go-grpc_opt=module=github.com/couchbase/stellar-nebula/genproto
 //go:generate protostellar proto/com.couchbase.v1.proto
-//go:generate protostellar proto/com.couchbase.data.v1.proto
+//go:generate protostellar proto/com.couchbase.kv.v1.proto
 //go:generate protostellar proto/com.couchbase.query.v1.proto
 //go:generate protostellar proto/com.couchbase.search.v1.proto
 //go:generate protostellar proto/com.couchbase.analytics.v1.proto
