3934793cd57161dd9ba190beafe7658eed2f1366
Brett Lawson <brett19@gmail.com>
2022-11-29 12:47:54 -0800

    Refactored proto directory structure to make Ruby generation work.

--- generate-protos.go
+++ generate-protos.go
@@ -1,14 +1,14 @@
 //go:generate mkdir -p ./genproto
-//go:generate -command protostellar protoc --go_out=./genproto --go_opt=module=github.com/couchbase/stellar-nebula/genproto --go-grpc_out=./genproto --go-grpc_opt=module=github.com/couchbase/stellar-nebula/genproto
-//go:generate protostellar proto/com.couchbase.v1.proto
-//go:generate protostellar proto/com.couchbase.kv.v1.proto
-//go:generate protostellar proto/com.couchbase.query.v1.proto
-//go:generate protostellar proto/com.couchbase.search.v1.proto
-//go:generate protostellar proto/com.couchbase.analytics.v1.proto
-//go:generate protostellar proto/com.couchbase.view.v1.proto
-//go:generate protostellar proto/com.couchbase.transactions.v1.proto
-//go:generate protostellar proto/com.couchbase.routing.v1.proto
-//go:generate protostellar proto/com.couchbase.admin.bucket.v1.proto
-//go:generate protostellar proto/com.couchbase.internal.hooks.v1.proto
+//go:generate -command protostellar protoc --proto_path=./proto --proto_path=./contrib/googleapis --go_out=./genproto --go_opt=module=github.com/couchbase/stellar-nebula/genproto --go-grpc_out=./genproto --go-grpc_opt=module=github.com/couchbase/stellar-nebula/genproto
+//go:generate protostellar couchbase/common.v1.proto
+//go:generate protostellar couchbase/kv.v1.proto
+//go:generate protostellar couchbase/query.v1.proto
+//go:generate protostellar couchbase/search.v1.proto
+//go:generate protostellar couchbase/analytics.v1.proto
+//go:generate protostellar couchbase/view.v1.proto
+//go:generate protostellar couchbase/transactions.v1.proto
+//go:generate protostellar couchbase/routing.v1.proto
+//go:generate protostellar couchbase/admin/bucket.v1.proto
+//go:generate protostellar couchbase/internal/hooks.v1.proto
 
 package main
