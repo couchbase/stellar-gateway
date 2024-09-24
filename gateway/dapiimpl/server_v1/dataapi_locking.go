f12ba4e413e70fa472797dfcd069ebbfd2329842
Brett Lawson <>
2024-09-23 18:16:07 -0700

    ING-907: Added support for locking operations to Data API.

---
+++ gateway/dapiimpl/server_v1/dataapi_locking.go
@@ -0,0 +1,123 @@
+package server_v1
+
+import (
+	"bytes"
+	"context"
+	"errors"
+
+	"github.com/couchbase/gocbcorex"
+	"github.com/couchbase/gocbcorex/memdx"
+	"github.com/couchbase/stellar-gateway/dataapiv1"
+)
+
+func (s *DataApiServer) LockDocument(
+	ctx context.Context, in dataapiv1.LockDocumentRequestObject,
+) (dataapiv1.LockDocumentResponseObject, error) {
+	bucketAgent, oboUser, errSt := s.authHandler.GetMemdOboAgent(ctx, in.Params.Authorization, in.BucketName)
+	if errSt != nil {
+		return nil, errSt.Err()
+	}
+
+	key, errSt := s.parseKey(in.DocumentKey)
+	if errSt != nil {
+		return nil, errSt.Err()
+	}
+
+	var opts gocbcorex.GetAndLockOptions
+	opts.OnBehalfOf = oboUser
+	opts.ScopeName = in.ScopeName
+	opts.CollectionName = in.CollectionName
+	opts.Key = key
+
+	if in.Body.LockTime != nil {
+		opts.LockTime = *in.Body.LockTime
+	}
+
+	result, err := bucketAgent.GetAndLock(ctx, &opts)
+	if err != nil {
+		if errors.Is(err, memdx.ErrDocLocked) {
+			return nil, s.errorHandler.NewDocLockedStatus(err, in.BucketName, in.ScopeName, in.CollectionName, in.DocumentKey).Err()
+		} else if errors.Is(err, memdx.ErrDocNotFound) {
+			return nil, s.errorHandler.NewDocMissingStatus(err, in.BucketName, in.ScopeName, in.CollectionName, in.DocumentKey).Err()
+		} else if errors.Is(err, memdx.ErrUnknownCollectionName) {
+			return nil, s.errorHandler.NewCollectionMissingStatus(err, in.BucketName, in.ScopeName, in.CollectionName).Err()
+		} else if errors.Is(err, memdx.ErrUnknownScopeName) {
+			return nil, s.errorHandler.NewScopeMissingStatus(err, in.BucketName, in.ScopeName).Err()
+		} else if errors.Is(err, memdx.ErrAccessError) {
+			return nil, s.errorHandler.NewCollectionNoWriteAccessStatus(err, in.BucketName, in.ScopeName, in.CollectionName).Err()
+		}
+		return nil, s.errorHandler.NewGenericStatus(err).Err()
+	}
+
+	resp := dataapiv1.LockDocument200AsteriskResponse{
+		Headers: dataapiv1.LockDocument200ResponseHeaders{
+			ETag:     casToHttpEtag(result.Cas),
+			XCBFlags: uint32(result.Flags),
+		},
+	}
+
+	contentType := flagsToHttpContentType(result.Flags)
+
+	contentEncoding, respValue, errSt :=
+		CompressHandler{}.MaybeCompressContent(result.Value, 0, in.Params.AcceptEncoding)
+	if errSt != nil {
+		return nil, errSt.Err()
+	}
+
+	resp.ContentType = contentType
+	resp.Headers.ContentEncoding = contentEncoding
+	resp.Body = bytes.NewReader(respValue)
+	resp.ContentLength = int64(len(respValue))
+
+	return resp, nil
+}
+
+func (s *DataApiServer) UnlockDocument(
+	ctx context.Context, in dataapiv1.UnlockDocumentRequestObject,
+) (dataapiv1.UnlockDocumentResponseObject, error) {
+	bucketAgent, oboUser, errSt := s.authHandler.GetMemdOboAgent(ctx, in.Params.Authorization, in.BucketName)
+	if errSt != nil {
+		return nil, errSt.Err()
+	}
+
+	key, errSt := s.parseKey(in.DocumentKey)
+	if errSt != nil {
+		return nil, errSt.Err()
+	}
+
+	cas, errSt := s.parseCAS(in.Params.IfMatch)
+	if errSt != nil {
+		return nil, errSt.Err()
+	}
+
+	var opts gocbcorex.UnlockOptions
+	opts.OnBehalfOf = oboUser
+	opts.ScopeName = in.ScopeName
+	opts.CollectionName = in.CollectionName
+	opts.Key = key
+	opts.Cas = cas
+
+	result, err := bucketAgent.Unlock(ctx, &opts)
+	if err != nil {
+		if errors.Is(err, memdx.ErrCasMismatch) {
+			return nil, s.errorHandler.NewDocCasMismatchStatus(err, in.BucketName, in.ScopeName, in.CollectionName, in.DocumentKey).Err()
+		} else if errors.Is(err, memdx.ErrDocNotLocked) {
+			return nil, s.errorHandler.NewDocNotLockedStatus(err, in.BucketName, in.ScopeName, in.CollectionName, in.DocumentKey).Err()
+		} else if errors.Is(err, memdx.ErrDocNotFound) {
+			return nil, s.errorHandler.NewDocMissingStatus(err, in.BucketName, in.ScopeName, in.CollectionName, in.DocumentKey).Err()
+		} else if errors.Is(err, memdx.ErrUnknownCollectionName) {
+			return nil, s.errorHandler.NewCollectionMissingStatus(err, in.BucketName, in.ScopeName, in.CollectionName).Err()
+		} else if errors.Is(err, memdx.ErrUnknownScopeName) {
+			return nil, s.errorHandler.NewScopeMissingStatus(err, in.BucketName, in.ScopeName).Err()
+		} else if errors.Is(err, memdx.ErrAccessError) {
+			return nil, s.errorHandler.NewCollectionNoWriteAccessStatus(err, in.BucketName, in.ScopeName, in.CollectionName).Err()
+		}
+		return nil, s.errorHandler.NewGenericStatus(err).Err()
+	}
+
+	return dataapiv1.UnlockDocument200Response{
+		Headers: dataapiv1.UnlockDocument200ResponseHeaders{
+			XCBMutationToken: tokenFromGocbcorex(in.BucketName, result.MutationToken),
+		},
+	}, nil
+}
