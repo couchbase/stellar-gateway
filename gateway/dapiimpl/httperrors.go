c163de8f77c2e11de4b2974546bf62510b55ba43
Brett Lawson <>
2024-10-16 17:19:01 -0700

    ING-935: Added support for various timing and counting metrics.
    This also refactors our error conversion to occur as a middleware such that
    appropriate error information is produced by our stats counting middleware.
    Additionally, we have updated our OpenTelemetry library version.

--- gateway/dapiimpl/httperrors.go
+++ gateway/dapiimpl/httperrors.go
@@ -1,21 +1,31 @@
 package dapiimpl
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"net/http"
 
 	"github.com/couchbase/stellar-gateway/gateway/dapiimpl/server_v1"
+	"github.com/oapi-codegen/runtime/strictmiddleware/nethttp"
+	"go.uber.org/zap"
 )
 
-func StatusErrorHttpHandler(w http.ResponseWriter, r *http.Request, err error) {
-	var errSt *server_v1.StatusError
-	if errors.As(err, &errSt) {
-		errBytes, _ := json.Marshal(errSt.S)
-		w.WriteHeader(errSt.S.StatusCode)
-		_, _ = w.Write(errBytes)
-		return
-	}
+func NewErrorHandler(logger *zap.Logger) func(f nethttp.StrictHTTPHandlerFunc, operationID string) nethttp.StrictHTTPHandlerFunc {
+	return func(f nethttp.StrictHTTPHandlerFunc, operationID string) nethttp.StrictHTTPHandlerFunc {
+		return func(ctx context.Context, w http.ResponseWriter, r *http.Request, request interface{}) (response interface{}, err error) {
+			resp, err := f(ctx, w, r, request)
+			if err != nil {
+				var errSt *server_v1.StatusError
+				if errors.As(err, &errSt) {
+					errBytes, _ := json.Marshal(errSt.S)
+					w.WriteHeader(errSt.S.StatusCode)
+					_, _ = w.Write(errBytes)
+					return nil, nil
+				}
+			}
 
-	http.Error(w, err.Error(), http.StatusInternalServerError)
+			return resp, err
+		}
+	}
 }
