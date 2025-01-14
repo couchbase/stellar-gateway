0270a46a68f5613ae669ef4235470a85169d7bdb
Brett Lawson <>
2025-01-14 13:51:05 -0800

    WIP: Don't fetch all meta-data

--- gateway/apiversion/metadata.go
+++ gateway/apiversion/metadata.go
@@ -2,20 +2,13 @@ package apiversion
 
 import (
 	"context"
-	"errors"
 	"time"
 
 	"google.golang.org/grpc/metadata"
 )
 
 func FromContext(ctx context.Context) (ApiVersion, error) {
-	md, _ := metadata.FromIncomingContext(ctx)
-	if md == nil {
-		// if there is no GRPC context, return an error
-		return 0, errors.New("not a grpc request context")
-	}
-
-	apiVersions := md.Get("X-API-Version")
+	apiVersions := metadata.ValueFromIncomingContext(ctx, "X-API-Version")
 	if len(apiVersions) == 0 {
 		// if the user has not specified a version, use the latest
 		return Latest, nil
