ea41da340d31003b24cca494b1ba064494d5dae6
Jack Westwood <jack.westwood@couchbase.com>
2025-02-12 15:53:58 +0000

    ING-1008: Support tracing through the proxy endpoints

--- gateway/dapiimpl/uatracing.go
+++ gateway/dapiimpl/uatracing.go
@@ -2,34 +2,25 @@ package dapiimpl
 
 import (
 	"context"
+	"net/http"
+
+	"go.opentelemetry.io/otel/propagation"
+
 	"github.com/oapi-codegen/runtime/strictmiddleware/nethttp"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/trace"
-	"net/http"
 )
 
-func NewUserAgentTracingHandler() func(f nethttp.StrictHTTPHandlerFunc, operationID string) nethttp.StrictHTTPHandlerFunc {
+func NewOtelTracingHandler() func(f nethttp.StrictHTTPHandlerFunc, operationID string) nethttp.StrictHTTPHandlerFunc {
 	return func(f nethttp.StrictHTTPHandlerFunc, operationID string) nethttp.StrictHTTPHandlerFunc {
 		return func(ctx context.Context, w http.ResponseWriter, r *http.Request, request interface{}) (response interface{}, err error) {
 			tp := otel.GetTextMapPropagator()
-			ctx = tp.Extract(r.Context(), propagation.HeaderCarrier(r.Header))
+			ctx = tp.Extract(ctx, propagation.HeaderCarrier(r.Header))
 
-			var tracer trace.Tracer
-			if span := trace.SpanFromContext(ctx); span.SpanContext().IsValid() {
-				tracer = newTracer(span.TracerProvider())
-			} else {
-				tracer = newTracer(otel.GetTracerProvider())
-			}
-
+			tracer := otel.GetTracerProvider().Tracer("github.com/couchbase/stellar-gateway/gateway/dapiimpl")
 			ctx, span := tracer.Start(ctx, operationID)
 			defer span.End()
 
 			return f(ctx, w, r, request)
 		}
 	}
-}
-
-func newTracer(tp trace.TracerProvider) trace.Tracer {
-	return tp.Tracer("github.com/couchbase/stellar-gateway/gateway/dapiimpl")
 }
