566ed773727cb7b8521c0634839be7a91ffe878b
Couchbase Build Team <build-team@couchbase.com>
2025-03-13 07:35:08 +0000

    CBD-6163: BSL License updates - Couchbase Cloud Native Gateway Version 1.1
    Change-Id: I717c70524a30f9215485360908e5742d77682b5b

--- contrib/cbconfig/cbconfig.go
+++ contrib/cbconfig/cbconfig.go
@@ -1,3 +1,13 @@
+/*
+Copyright 2022-Present Couchbase, Inc.
+
+Use of this software is governed by the Business Source License included in
+the file licenses/BSL-Couchbase.txt.  As of the Change Date specified in that
+file, in accordance with the Business Source License, use of this software will
+be governed by the Apache License, Version 2.0, included in the file
+licenses/APL2.txt.
+*/
+
 package cbconfig
 
 // TODO(brett19): We should switch to using OpenAPI v3 to define the ns_server REST here...
